c0ed8ad05b043934c26c7303666139f859f9d1b4
LoxiasMcF <seko1231@gmail.com>
2022-11-17 12:38:49 +0100

    UI, WinScreen V1 & Car Fix

--- Far drive/Assets/Scripts/ScriptGameController.cs
+++ Far drive/Assets/Scripts/ScriptGameController.cs
@@ -1,22 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
 
 public class ScriptGameController : MonoBehaviour
 {
+    public TMP_Text uIText;
+    public Scene victoryScene;
+
     public bool goGetMilk = false;
     public bool gotMilk = false;
     public bool gotHome = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void EditObjective() {
+        if (goGetMilk == true)
+            uIText.text = "Go get milk at the store";
+        if (gotMilk == true)
+            uIText.text = "Bring the milk home";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void GoToEndScreen() {
+        if (gotHome == true)
+            SceneManager.LoadScene("Victory Scene", LoadSceneMode.Single);
     }
 }
--- Far drive/Assets/Scripts/Triggers/ScriptGotMilkTrigger.cs
+++ Far drive/Assets/Scripts/Triggers/ScriptGotMilkTrigger.cs
@@ -2,22 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScriptGoalTrigger : MonoBehaviour
+public class ScriptGotMilkTrigger : MonoBehaviour
 {
     public ScriptGameController gameController;
     public GameObject playerCar;
     BoxCollider2D playerCollider;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         playerCollider = playerCar.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D other){
-        if (gameController.gotMilk == true) {
-            gameController.gotHome = true;
-            gameController.GoToEndScreen();
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (gameController.goGetMilk == true) {
+            gameController.gotMilk = true;
+            gameController.EditObjective();
         }
+            
+        
     }
 }
